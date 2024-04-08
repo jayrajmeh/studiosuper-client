d431e2d5fe9443ea319880332dd2f7fb3d419332
jayrajmeh <141761329+jayrajmeh@users.noreply.github.com>
2024-04-08 22:21:03 +0530

    set url and page issue

--- src/app/pages/Student/Student_List.jsx
+++ src/app/pages/Student/Student_List.jsx
@@ -586,9 +586,9 @@ const handleFileChange = async(event) => {
                     <option value={20}>20</option>
                     <option value={30}>30</option>
                     <option value={50}>50</option>
-                    <option value={50}>100</option>
-                    <option value={50}>150</option>
-                    <option value={50}>200</option>
+                    <option value={100}>100</option>
+                    <option value={150}>150</option>
+                    <option value={200}>200</option>
 
 
                   </select>
